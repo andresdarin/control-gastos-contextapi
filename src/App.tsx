dcb05674167171da1b53cf76a75ab9f665c248c3
Andrés Darín <andres.darin05@gmail.com>
2025-10-19 20:09:48 -0300

    feat: add initial project setup with SVG icons and budget form component
    - Added SVG icons for expenses, leisure, health, subscriptions, and Vite logo.
    - Created main application structure with App component.
    - Implemented BudgetForm component for budget input.
    - Configured Tailwind CSS for styling.
    - Set up TypeScript configuration files for app and node.
    - Integrated Vite with React and Tailwind CSS plugins.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import BudgetForm from "./components/BudgetForm"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-blue-600 py-8 max-h-72">
+        <h1 className="text-white text-4xl font-black text-center uppercase">Planificador de gastos</h1>
+
+      </div>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgetForm />
+      </div>
+    </>
+  )
+}
+
+export default App
